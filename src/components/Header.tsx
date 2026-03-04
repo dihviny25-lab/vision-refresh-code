b9c6ff310f50cb7fb461eecde5e28cb5e429d1cf
dihviny25-lab <dihviny25@gmail.com>
2026-03-04 12:25:35 -0300

    Update logo image size in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,7 +41,7 @@ const Header = () => {
     >
       <div className="container-vision flex items-center justify-between h-16 md:h-20">
         <Link to="/" className="flex items-center gap-2.5" aria-label="Igreja Vision - Página inicial">
-          <img src={logoDark} alt="Igreja Vision" className="h-8 md:h-9 w-auto" />
+          <img src={logoDark} alt="Igreja Vision" className="h-10 md:h-12 w-auto" />
         </Link>
 
         <div className="flex items-center gap-3">
